c80444fe63369ac4f35be0370da8d6075b3a3a3f
Deven Gonyer <79484636+GyroidGaming@users.noreply.github.com>
2026-07-03 06:47:29 -0400

    All progress as of 07/02/2026

---
+++ pub-brawl-simulator/rigid_player.gd
@@ -0,0 +1,26 @@
+extends RigidBody3D
+
+@onready var neck := $Neck
+@onready var camera := $Neck/Camera3D
+
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			neck.rotate_y(-event.relative.x * 0.01)
+			camera.rotate_x(-event.relative.y * 0.01)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+
+func _physics_process(delta: float) -> void:
+	var input := Vector3.ZERO
+	input.x = Input.get_axis("move_left", "move_right")
+	input.z = Input.get_axis("move_forward", "move_back")
+	
+	apply_central_force(input * 1200.0 * delta)
